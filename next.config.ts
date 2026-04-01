76d337685223c259cf4505ed5777841f95be6eea
Ashif Ahmed Shuvo <shuvoasifahmed@gmail.com>
2026-04-02 03:57:04 +0600

    refactor: Configure Next.js image optimization
    Set `images.unoptimized` to `true` in `next.config.ts` to disable image optimization and ensure correct rendering of remote placeholder images.

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
   },
   // Allow access to remote image placeholder.
   images: {
-    unoptimized: true,
+    unoptimized: true ,
     remotePatterns: [
       {
         protocol: 'https',
